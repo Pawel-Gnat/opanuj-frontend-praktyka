41ce0314506ea4c5dd05d623ddd8ecdb5ffdd6b0
Paweł <pawel_gnat@o2.pl>
2024-02-20 21:20:34 +0100

    module1/lesson1/validate-it

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,34 +1,35 @@
-function validator() {
-  const input = document.getElementById('input');
-  const button = document.getElementById('button');
-  const button2 = document.getElementById('button2');
-  const result = document.getElementById('result');
+const validateBtn = document.getElementById('validateBtn');
+const clearBtn = document.getElementById('clearBtn');
+const textInput = document.getElementById('input');
+const resultText = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
-    }
-  });
-
-  button2.addEventListener('click', () => {
-    input.value = '';
-    result.innerHTML = '';
-  });
+function renderResultMessage(element, text) {
+  element.textContent = text;
 }
 
-validator();
+const isNotEmpty = (value) => value.trim() !== '';
+const isNumberInRange = (value, min, max) => {
+  return value >= min && value <= max;
+};
+
+function validateValue(value, rules) {
+  for (const rule of rules) {
+    if (!rule(value)) {
+      return renderResultMessage(resultText, 'Invalid');
+    }
+  }
+  return renderResultMessage(resultText, 'Valid');
+}
+
+function clearResultAndInputValue() {
+  textInput.value = '';
+  resultText.textContent = '';
+}
+
+validateBtn.addEventListener('click', () =>
+  validateValue(textInput.value, [
+    isNotEmpty,
+    (value) => isNumberInRange(value, 0, 100),
+  ])
+);
+clearBtn.addEventListener('click', () => clearResultAndInputValue());
